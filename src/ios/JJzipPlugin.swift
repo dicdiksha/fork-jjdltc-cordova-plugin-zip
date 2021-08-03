ff1a2c08b364da2d83315527295b1ea7b14b84d8
Harish Kumar Gangula <harish@sanketika.in>
2021-08-03 09:33:27 +0530

    Unzip method implemented

--- src/ios/JJzipPlugin.swift
+++ src/ios/JJzipPlugin.swift
@@ -1,16 +1,8 @@
-//
-//  JJzipPlugin.swift
-//
-//
-//  Created by Pace Wisdom on 01/07/21.
-//
 import Foundation
 import SSZipArchive
-
-
 @objc(JJzip) class JJzip : CDVPlugin   {
     
-   //ZiP Method.
+    //ZiP Method.
     @objc func zip(_ command: CDVInvokedUrlCommand?) {
         var pluginResult: CDVPluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
         let directoriesToBeSkipped = command?.arguments[0] as? String
@@ -20,90 +12,76 @@ import SSZipArchive
         
     }
     
-//UnZip Method.
- @objc func unzip(_ command: CDVInvokedUrlCommand?) {
-    
-    do {
-        let sourceDictionary = getSourceDictionary(command?.arguments[0] as? String)
-        let targetOptions = command?.arguments[1]
-        var targetPath = (targetOptions! as AnyObject).value(forKey: "target") as! String
-        targetPath = targetPath.replacingOccurrences(of: "file://", with: "")
-        let sourcePath = sourceDictionary!["path"] as! String
-        let sourceName = sourceDictionary!["name"] as! String
+    //UnZip Method.
+     @objc func unzip(_ command: CDVInvokedUrlCommand) {
+        
+            let sourceDirectory =  command.arguments[0] as? String
+            guard let sourceDirectoryPath = sourceDirectory else {
+                let responseObj = [
+                    "success": false,
+                    "message": "decompress Operation fail due to source directory path is nil",
+                ] as [String : Any]
 
-            let success = SSZipArchive.unzipFile(
-                atPath: (sourcePath ?? "") + (sourceName ?? ""),
-                toDestination: targetPath
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: responseObj)
+                commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                return
+            }
+            let sourceDictionary = getSourceDictionary(sourceDirectoryPath)
+            
+            let targetOptions = command.arguments[1]
+            let targetPath = (targetOptions as AnyObject).value(forKey: "target") as? String
+            
+            guard let targetPathString = targetPath else {
+                let responseObj = [
+                    "success": false,
+                    "message": "decompress Operation fail due to destination directory path is nil",
+                ] as [String : Any]
+
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: responseObj)
+                commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                return
+            }
+            
+            let sourcePath = sourceDictionary!["path"] as! String
+            let sourceName = sourceDictionary!["name"] as! String
+
+            let success: Bool = SSZipArchive.unzipFile(
+                    atPath: sourcePath + sourceName,
+                    toDestination: targetPathString.replacingOccurrences(of: "file://", with: "")
             )
 
+            if (success == false) {
+                let responseObj = [
+                    "success": false,
+                    "message": "decompress Operation fail",
+                ] as [String : Any]
+
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: responseObj)
+                commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                return
+            }
 
             let responseObj = [
                 "success": true,
                 "message": "decompress Operation success"
             ] as [String : Any]
 
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: responseObj)
-            commandDelegate.send(pluginResult, callbackId: command?.callbackId)
-    } catch let error {
-        let responseObj = [
-            "success": false,
-            "message": "decompress Operation fail with \(error.localizedDescription)",
-        ] as [String : Any]
-
-    let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: responseObj)
-        commandDelegate.send(pluginResult, callbackId: command?.callbackId)
-    }
-    
-    
-}
-
-
-// getSourceDictionary Method.
-@objc func getSourceDictionary(_ sourceString: String?) -> [AnyHashable : Any]? {
-    let lastIndexSlash = (sourceString as NSString?)?.range(of: "/", options: .backwards).location ?? 0
-    let path = (sourceString as NSString?)?.substring(with: NSRange(location: 0, length: lastIndexSlash + 1))
-    let name = (sourceString as NSString?)?.substring(from: lastIndexSlash + 1)
-    let sourceDictionary = [
-        "path": path?.replacingOccurrences(of: "file://", with: "") ?? "",
-        "name": name ?? ""
-    ]
-    return sourceDictionary
-}
-
-
-
-@objc func jsEvent(_ event: String?, _ data: String?) {
-    var eventStrig = "cordova.fireDocumentEvent('\(event ?? "")'"
-    // NSString *eventStrig = [NSString stringWithFormat:@"console.log('%@'", event];
-
-    if let data = data {
-        eventStrig = "\(eventStrig),\(data)"
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: responseObj)
+                commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        
     }
 
-    eventStrig = eventStrig + ");"
 
-    commandDelegate.evalJs(eventStrig)
-}
-
-
-@objc func dictionary(toJSONString toCast: [AnyHashable : Any]?) -> String? {
-    var error: Error?
-    var jsonData: Data? = nil
-    do {
-        if let toCast = toCast {
-            jsonData = try JSONSerialization.data(withJSONObject: toCast, options: .prettyPrinted)
-        }
-    } catch {
+    // getSourceDictionary Method.
+    @objc func getSourceDictionary(_ sourceString: String?) -> [AnyHashable : Any]? {
+        let lastIndexSlash = (sourceString as NSString?)?.range(of: "/", options: .backwards).location ?? 0
+        let path = (sourceString as NSString?)?.substring(with: NSRange(location: 0, length: lastIndexSlash + 1))
+        let name = (sourceString as NSString?)?.substring(from: lastIndexSlash + 1)
+        let sourceDictionary = [
+            "path": path?.replacingOccurrences(of: "file://", with: "") ?? "",
+            "name": name ?? ""
+        ]
+        return sourceDictionary
     }
-    if jsonData == nil {
-        return nil
-    } else {
-        if let jsonData = jsonData {
-            return String(data: jsonData, encoding: .utf8)
-        }
-        return nil
-    }
-}
 
-    
 }
